063a2a77bafe951264a98aa82e699f09cab27859
Dmitrii <heybehonestproduction@gmail.com>
2023-06-06 19:26:31 +0200

    fix diff.test.js

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -1,13 +1,11 @@
 import { test, expect } from '@jest/globals';
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
+import path from 'path';
 import fs from 'fs';
 import genDiff from '../src/index.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.resolve();
 
-const getPath = (filename) => join(__dirname, '..', '__fixtures__', filename);
+const getPath = (filename) => path.join(__dirname, '__fixtures__', filename);
 
 const resultPathStylish = getPath('resultStylish.txt');
 const stylish = fs.readFileSync(resultPathStylish, 'utf-8');
@@ -37,36 +35,4 @@ test('generate difference between YMAL/YML files', () => {
 test('generate difference between YMAL and JSON files', () => {
   expect(genDiff(getPath('file1.json'), getPath('file2.yml'))).toEqual(stylish);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import * as path from 'path';
-// import { readFileSync } from 'fs';
-// import genDiff from '../src/index.js';
-// import { expect, test } from '@jest/globals';
-// import process from 'process';
-
-// const firstAnswerPath = path.resolve(process.cwd(), '__fixtures__/JSONExpectedResult.txt');
-// const firstAnswer = readFileSync(firstAnswerPath, 'utf8');
-
-
-// test('genDiff JSON plain', () => {
-//   expect(genDiff('file1.json', 'file2.json')).toEqual(firstAnswer);
-// });
-
-// test('genDiff YML plain', () => {
-//   expect(genDiff('file1.yml', 'file2.yml')).toEqual(firstAnswer);
-// });
-
 
