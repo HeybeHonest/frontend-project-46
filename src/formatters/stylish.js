a19d1c7b5aa4624c97dbb292c7f1bc8119474e92
Dmitrii <heybehonestproduction@gmail.com>
2023-06-07 14:41:47 +0200

    fix hexlet-check errors

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -10,38 +10,31 @@ const formatValue = (value, depth) => {
   return `{\n${lines.join('\n')}\n${getIndent(depth)}}`;
 };
 
-const formatNode = (node, depth = 0) => {
-  const indent = getIndent(depth);
-  const {
-    type,
-    key,
-    value,
-    oldValue,
-    newValue,
-    children,
-  } = node;
-
-  switch (type) {
-    case 'added':
-      return `${indent}  + ${key}: ${formatValue(value, depth + 1)}`;
-    case 'deleted':
-      return `${indent}  - ${key}: ${formatValue(value, depth + 1)}`;
-    case 'unchanged':
-      return `${indent}    ${key}: ${formatValue(value, depth + 1)}`;
-    case 'changed':
-      return [
-        `${indent}  - ${key}: ${formatValue(oldValue, depth + 1)}`,
-        `${indent}  + ${key}: ${formatValue(newValue, depth + 1)}`,
-      ].join('\n');
-    case 'nested':
-      return `${indent}    ${key}: ${formatDiff(children, depth + 1)}`;
-    default:
-      throw new Error(`Unknown node type: ${type}`);
-  }
-};
-
 const formatDiff = (diff, depth = 0) => {
   const indent = getIndent(depth);
+  const formatNode = (node, depth = 0) => {
+    const indent = getIndent(depth);
+    const { type, key, value, oldValue, newValue, children } = node;
+
+    switch (type) {
+      case 'added':
+        return `${indent}  + ${key}: ${formatValue(value, depth + 1)}`;
+      case 'deleted':
+        return `${indent}  - ${key}: ${formatValue(value, depth + 1)}`;
+      case 'unchanged':
+        return `${indent}    ${key}: ${formatValue(value, depth + 1)}`;
+      case 'changed':
+        return [
+          `${indent}  - ${key}: ${formatValue(oldValue, depth + 1)}`,
+          `${indent}  + ${key}: ${formatValue(newValue, depth + 1)}`,
+        ].join('\n');
+      case 'nested':
+        return `${indent}    ${key}: ${formatDiff(children, depth + 1)}`;
+      default:
+        throw new Error(`Unknown node type: ${type}`);
+    }
+  };
+
   const lines = diff.map((node) => formatNode(node, depth));
   return `{\n${lines.join('\n')}\n${indent}}`;
 };
