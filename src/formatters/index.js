22262a99e1ae5257b221f2d3d9118f5f0024481a
Dmitrii <heybehonestproduction@gmail.com>
2023-06-09 15:48:43 +0200

    mentor tasks

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,18 +1,19 @@
-import formatStylish from "./stylish.js";
-import formatJSON from "./json.js";
-import formatPlain from "./plain.js";
+import formatStylish from './stylish.js';
+import formatJSON from './json.js';
+import formatPlain from './plain.js';
 
 const formatDiff = (diff, format) => {
-    switch (format) {
-      case 'stylish':
-        return formatStylish(diff);
-      case 'plain':
-        return formatPlain(diff);
-      case 'json':
-        return formatJSON(diff);
-      default:
-        throw new Error(`Unsupported format: ${format}`);
+  switch (format) {
+    case 'stylish':
+      return formatStylish(diff);
+    case 'plain':
+      return formatPlain(diff);
+    case 'json':
+      return formatJSON(diff);
+    default:
+      throw new Error(`Unsupported format: ${format}`);
     }
   };
 
   export default formatDiff;
+  
