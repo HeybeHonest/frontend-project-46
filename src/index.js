6d21cf33e6c068b82c32b33920db7ce1e4d27787
Dmitrii <heybehonestproduction@gmail.com>
2023-06-09 15:39:56 +0200

    mentor tasks

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-import { getAbsolutePath, getParsedData, calculateDiff } from './utils.js';
+import { getParsedData, calculateDiff } from './utils.js';
 import formatDiff from './formatters/index.js';
 
-const generateDiff = (fileName1, fileName2, format = 'stylish') => {
-  const filePath1 = getAbsolutePath(fileName1);
-  const filePath2 = getAbsolutePath(fileName2);
-  const data1 = getParsedData(filePath1);
-  const data2 = getParsedData(filePath2);
-  const diff = calculateDiff(data1, data2);
+export default (filepath1, filepath2, format = 'stylish') => {
+  const obj1 = getParsedData(filepath1);
+  const obj2 = getParsedData(filepath2);
+  const diff = calculateDiff(obj1, obj2);
   return formatDiff(diff, format);
 };
-
-export default generateDiff;
--- src/utils.js
+++ src/utils.js
@@ -8,7 +8,6 @@ const fixturesDirPath = path.resolve(process.cwd(), '__fixtures__');
 
 const getAbsolutePath = (filePath) => path.resolve(fixturesDirPath, filePath);
 
-
 const readFile = (filePath) => fs.readFileSync(getAbsolutePath(filePath), 'utf-8');
 
 const getParsedData = (filePath) => {
